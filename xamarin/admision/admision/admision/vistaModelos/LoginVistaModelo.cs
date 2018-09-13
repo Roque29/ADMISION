a4ee869c749122c8d4792661cd5667e6492cf222
Elson Roque Caldas Dominguez <roque.unt@gmail.com>
2018-09-12 23:25:11 -0500

    Initial commit

---
+++ xamarin/admision/admision/admision/vistaModelos/LoginVistaModelo.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace admision.vistaModelos
+{
+    using GalaSoft.MvvmLight.Command;
+    using System.ComponentModel;
+    using System.Windows.Input;
+    using Xamarin.Forms;
+
+    class LoginVistaModelo : BaseVistaModelo
+    {
+
+
+        #region atributos
+        private string txtClaveCambiar;
+        private bool indicadorCambiar;
+        #endregion
+
+        #region propiedades 
+        public String txtUsuario
+        {
+            get;
+            set;
+        }
+        public String txtClave
+        {
+            get { return this.txtClaveCambiar; }
+            set { SetValue(ref this.txtClaveCambiar,value); }
+        }
+        public bool indicador
+        {
+            get { return this.indicadorCambiar; }
+            set { SetValue(ref this.indicadorCambiar, value); }
+        }
+        #endregion
+
+        #region constructor
+        public LoginVistaModelo()
+        {
+            this.txtUsuario = string.Empty;
+            this.txtClave = string.Empty;
+            this.indicador = false;
+        }
+        #endregion
+
+
+        #region comandos
+        public ICommand aceptarCommand
+        {
+            get
+            {
+                return new RelayCommand(aceptar);
+            }
+
+        }
+        private async void aceptar()
+        {
+            this.indicador = true;
+            if (string.IsNullOrEmpty(this.txtUsuario) || string.IsNullOrEmpty(this.txtClave))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "los campos usuario y clave son obligatorios",
+                    "Aceptar");
+                return;
+            }
+
+        }
+        #endregion
+
+    }
+
+}
